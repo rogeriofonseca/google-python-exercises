812120b3a90755ccb9e0458c66e3b5866f3c7b8e
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-09-06 02:01:37 -0300

    broken mix_up()

--- basic/string1.py
+++ basic/string1.py
@@ -66,7 +66,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
     # +++your code here+++
-    return
+    return ' '.join((a.replace(a[2],b[2]),b.replace(b[2],a[2])))
 
 
 # Provided simple test() function used in main() to print
